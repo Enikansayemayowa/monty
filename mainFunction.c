22515cb7494c3acc049750a222e590d631f2c2f1
Enikansayemayowa <enikansayemayowa405@gmail.com>
2023-08-24 09:05:39 -0700

    Implement the push and pall opcodes.

---
+++ mainFunction.c
@@ -0,0 +1,67 @@
+#include "monty.h"
+int main(int argc, char *argv[])
+{
+    FILE *f;
+    char *line = NULL;
+    size_t len = MAX_LINE_LENGTH;
+   /* ssize_t read;*/
+    stack_t *stack = NULL;
+    unsigned int line_number = 0;
+    char *opcode;
+    int value;
+    char *arg;
+
+    if (argc != 2)
+    {
+        fprintf(stderr, "USAGE: monty file\n");
+        exit(EXIT_FAILURE);
+    }
+
+    f = fopen(argv[1], "r");
+    if (f == NULL)
+    {
+        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+        exit(EXIT_FAILURE);
+    }
+
+    line = malloc(len + 1);
+    while (fgets(line, len, f))
+    {
+        line_number++;
+        opcode = strtok(line, " \n");
+
+        if (opcode == NULL)
+        {
+            continue;
+        }
+        else if (strcmp(opcode, "push") == 0)
+        {
+            arg = strtok(NULL, " \n");
+            if (arg == NULL)
+            {
+                fprintf(stderr, "L%d: usage: push integer\n", line_number);
+                exit(EXIT_FAILURE);
+            }
+            value = atoi(arg);
+            push(&stack, value);
+        }
+        else if (strcmp(opcode, "pall") == 0)
+        {
+            pall(&stack);
+        }
+        else if (strcmp(opcode, "pint") == 0)
+        {
+            pint(&stack, line_number);
+        }
+        else
+        {
+            fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    fclose(f);
+    free(line);
+
+    return 0;
+}
---
+++ push.c
@@ -0,0 +1,53 @@
+#include "monty.h"
+/*
+ * push - The opcode push pushes an element to the stack
+ * @stack: ponter to the pointer
+ * @number: line number to the item to push to
+ * @value: value to push to
+ * Return: 0
+ */
+void push(stack_t **stack, int value)
+{
+	stack_t *new_node;
+       new_node	= malloc(sizeof(stack_t));
+    if (new_node == NULL)
+    {
+        fprintf(stderr, "Error: malloc failed\n");
+        exit(EXIT_FAILURE);
+    }
+
+    new_node->n = value;
+    new_node->prev = NULL;
+    new_node->next = *stack;
+
+    if (*stack != NULL)
+    {
+        (*stack)->prev = new_node;
+    }
+
+    *stack = new_node;
+}
+/*
+ * pall - The opcode pall prints all the values on the stack, starting from the top of the stack.
+ * @stack: ponter to the pointer
+ * @number: line number to the item to push to
+ * Return: 0
+ */
+void pall(stack_t **stack)
+{
+    stack_t *current = *stack;
+    while (current != NULL)
+    {
+        printf("%d\n", current->n);
+        current = current->next;
+    }
+}
+void pint(stack_t **stack, unsigned int line_number)
+{
+    if (*stack == NULL)
+    {
+        fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+    printf("%d\n", (*stack)->n);
+}
